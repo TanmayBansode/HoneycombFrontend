f751d5fd9b56b5d09a39082f68b91658ce2994e2
TanmayBansode <tanmaysbansode@gmail.com>
2024-09-28 20:13:37 +0530

    minor change

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import HomeButton from "./components/HomeButton";
-import NavBar from "./components/NavBar";
+// import NavBar from "./components/NavBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
